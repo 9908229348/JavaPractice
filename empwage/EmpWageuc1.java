3766a7ed905f7d96ce2955d0ca94872db634d9f7
9908229348 <kalavaisravanthi98@gmail.com>
2021-09-24 18:08:54 +0530

    Added:Functionality to check Whether Employee Present or Not

---
+++ empwage/EmpWageuc1.java
@@ -0,0 +1,14 @@
+package empwage;
+
+public class EmpWageuc1 {
+	public static void main(String[] args) {
+	int is_FULL=1;
+	double empCheck=Math.floor(Math.random()*10)%2;
+	if(empCheck==is_FULL)
+	   System.out.println("Employee is Present");
+	   else
+		   System.out.println("Employee is absent");
+	   
+	}
+
+}
